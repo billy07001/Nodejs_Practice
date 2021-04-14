c7a7d56618c402f48c9b1a4bbe54a6aacc646e31
billy07001 <72130484+billy07001@users.noreply.github.com>
2021-04-14 17:50:47 +0800

    sec

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
         var pathName = url.parse(request.url).pathName;
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Request for" + pathName + " received.");
-        response.end();
+        route(pathName);
 
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Request for " + pathName + " received.");
+        response.end();
     }
     http.createServer(onRequest).listen(8880);
 }
